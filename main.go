17a7069b271f7d2b0abbf681d9d3aa61d5b76019
Atsushi NAGASE <a@ngs.io>
2025-08-12 22:53:49 +0900

    Support ruby

--- main.go
+++ main.go
@@ -7,10 +7,45 @@ import (
 	"html"
 	"io"
 	"os"
+	"strings"
 
 	"github.com/go-shiori/go-epub"
 	"go.ngs.io/jplaw-xml"
 )
+
+// processRubyElements converts Ruby elements to HTML ruby tags
+func processRubyElements(rubies []jplaw.Ruby) string {
+	var result strings.Builder
+	for _, ruby := range rubies {
+		if len(ruby.Rt) > 0 {
+			result.WriteString("<ruby>")
+			result.WriteString(html.EscapeString(ruby.Content))
+			for _, rt := range ruby.Rt {
+				result.WriteString("<rt>")
+				result.WriteString(html.EscapeString(rt.Content))
+				result.WriteString("</rt>")
+			}
+			result.WriteString("</ruby>")
+		} else {
+			result.WriteString(html.EscapeString(ruby.Content))
+		}
+	}
+	return result.String()
+}
+
+// processTextWithRuby processes mixed content (text + Ruby elements)
+func processTextWithRuby(content string, rubies []jplaw.Ruby) string {
+	if len(rubies) == 0 {
+		return html.EscapeString(content)
+	}
+	
+	var result strings.Builder
+	if content != "" {
+		result.WriteString(html.EscapeString(content))
+	}
+	result.WriteString(processRubyElements(rubies))
+	return result.String()
+}
 
 func main() {
 	destPath := flag.String("d", "", "Destination file path")
@@ -47,6 +82,7 @@ func main() {
 		os.Exit(1)
 	}
 
+	// Use plain text for EPUB title (no HTML markup allowed)
 	book, err := epub.NewEpub(data.LawBody.LawTitle.Content)
 	if err != nil {
 		fmt.Printf("Error creating epub: %v\n", err)
@@ -55,6 +91,45 @@ func main() {
 
 	book.SetAuthor(data.LawNum)
 	book.SetLang(string(data.Lang))
+	
+	// Add CSS for Ruby text rendering
+	rubyCSSContent := `
+/* Ruby text styling for Japanese phonetic guides */
+ruby {
+	ruby-align: center;
+	ruby-position: over;
+}
+
+rt {
+	font-size: 0.6em;
+	line-height: 1;
+	text-align: center;
+	color: #666;
+}
+
+/* Fallback for older EPUB readers */
+ruby > rt {
+	display: inline-block;
+	font-size: 0.6em;
+	line-height: 1;
+	text-align: center;
+	vertical-align: top;
+	color: #666;
+}
+
+/* Modern browsers and readers */
+@supports (ruby-position: over) {
+	ruby {
+		ruby-position: over;
+	}
+}
+`
+	rubyCSS, err := book.AddCSS(rubyCSSContent, "ruby.css")
+	if err != nil {
+		fmt.Printf("Error adding Ruby CSS: %v\n", err)
+		os.Exit(1)
+	}
+	_ = rubyCSS // Mark as used
 	eraStr := ""
 	switch data.Era {
 	case jplaw.EraMeiji:
@@ -70,12 +145,15 @@ func main() {
 	}
 	description := fmt.Sprintf("公布日: %s %d年%d月%d日", eraStr, data.Year, data.PromulgateMonth, data.PromulgateDay)
 	description += fmt.Sprintf("\n法令番号: %s", data.LawNum)
-	description += fmt.Sprintf("\n現行法令名: %s %s", data.LawBody.LawTitle.Content, data.LawBody.LawTitle.Kana)
+	// Include Ruby text in description for better accessibility
+	lawTitleWithRuby := processTextWithRuby(data.LawBody.LawTitle.Content, data.LawBody.LawTitle.Ruby)
+	description += fmt.Sprintf("\n現行法令名: %s %s", lawTitleWithRuby, data.LawBody.LawTitle.Kana)
 	book.SetDescription(description)
 
 	for i, chapter := range data.LawBody.MainProvision.Chapter {
 		filename := fmt.Sprintf("chapter-%d.xhtml", i)
-		body := fmt.Sprintf("<h2>%s</h2>", html.EscapeString(chapter.ChapterTitle.Content))
+		chapterTitleHTML := processTextWithRuby(chapter.ChapterTitle.Content, chapter.ChapterTitle.Ruby)
+		body := fmt.Sprintf("<h2>%s</h2>", chapterTitleHTML)
 		filename, err = book.AddSection(body, chapter.ChapterTitle.Content, filename, "")
 		if err != nil {
 			fmt.Printf("Error adding section: %v\n", err)
@@ -83,19 +161,31 @@ func main() {
 		}
 		for j, article := range chapter.Article {
 			subFilename := fmt.Sprintf("article-%d-%d.xhtml", i, j)
-			articleCaption := ""
+			articleTitleHTML := processTextWithRuby(article.ArticleTitle.Content, article.ArticleTitle.Ruby)
+			articleCaptionHTML := ""
 			if article.ArticleCaption != nil {
-				articleCaption = article.ArticleCaption.Content
+				articleCaptionHTML = processTextWithRuby(article.ArticleCaption.Content, article.ArticleCaption.Ruby)
 			}
-			articleTitle := fmt.Sprintf("%s %s", article.ArticleTitle.Content, articleCaption)
-			body := fmt.Sprintf("<h3>%s</h3><ol>", html.EscapeString(articleTitle))
+			var articleTitleFull string
+			if articleCaptionHTML != "" {
+				articleTitleFull = fmt.Sprintf("%s %s", articleTitleHTML, articleCaptionHTML)
+			} else {
+				articleTitleFull = articleTitleHTML
+			}
+			body := fmt.Sprintf("<h3>%s</h3><ol>", articleTitleFull)
 			for _, para := range article.Paragraph {
 				for _, sentense := range para.ParagraphSentence.Sentence {
-					body += fmt.Sprintf("<li>%s</li>", html.EscapeString(sentense.Content))
+					sentenceHTML := processTextWithRuby(sentense.Content, sentense.Ruby)
+					body += fmt.Sprintf("<li>%s</li>", sentenceHTML)
 				}
 			}
 			body += "</ol>"
-			_, err = book.AddSubSection(filename, body, articleTitle, subFilename, "")
+			// Use plain text for table of contents
+			articleTitlePlain := article.ArticleTitle.Content
+			if article.ArticleCaption != nil {
+				articleTitlePlain = fmt.Sprintf("%s %s", article.ArticleTitle.Content, article.ArticleCaption.Content)
+			}
+			_, err = book.AddSubSection(filename, body, articleTitlePlain, subFilename, "")
 			if err != nil {
 				fmt.Printf("Error adding section: %v\n", err)
 				os.Exit(1)
